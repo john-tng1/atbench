6f213755a602ba1269e82c81ec3b98293acfa150
john-tng1 <ngn.tanh@gmail.com>
2021-11-07 23:08:41 +1100

    feat(component): add Serializers for API & Add RoomView with generic.ListAPIView
    Sample dataclass Room to be changed later

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,8 @@
 
 from django.urls import path
-from .views import main
+from .views import RoomView
 
 
 urlpatterns = [
-    path('', main),
-    path('home', main)
+    path('room', RoomView.as_view())
 ]
